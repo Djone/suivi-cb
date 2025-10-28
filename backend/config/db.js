22676731179b697e0061f01ecce0f0a98cfa330a
Djone <jonathan.guesnier@gmail.com>
2025-10-28 14:47:43 +0100

    Modification pour différencier les 2 types de BDD (Prod et dev)

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const path = require('path');
 
 // Le chemin de la base de données est déterminé par la variable d'environnement DB_PATH.
 // Si elle n'est pas définie (en dev), on utilise un chemin par défaut vers le dossier /data à la racine du projet.
-const dbPath = process.env.DB_PATH || path.join(__dirname, '../../data/database.db');
+const isProduction = process.env.NODE_ENV === 'production';
+// En production, utilise `database.db`. En développement, utilise `database.dev.db`.
+const dbFilename = isProduction ? 'database.db' : 'database.dev.db';
+const dbPath = process.env.DB_PATH || path.join(__dirname, '../../data', dbFilename);
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
