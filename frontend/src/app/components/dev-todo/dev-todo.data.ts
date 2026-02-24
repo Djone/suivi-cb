6625564e61be1e25e8db757e39f837f8ba343e24
Djone <jonathan.guesnier@gmail.com>
2026-02-24 18:04:47 +0100

    Nouvelles fonctionnalités 1.3.0

--- frontend/src/app/components/dev-todo/dev-todo.data.ts
+++ frontend/src/app/components/dev-todo/dev-todo.data.ts
@@ -5,21 +5,21 @@ export const DEV_TODO_ITEMS: DevTodoItem[] = [
     title: '[Ticket 4.1] Tableau de salaires : frontend',
     description: ['[ ] Integrer la logique frontend'],
     status: 'in-progress',
-    targetVersion: '1.3.0',
+    targetVersion: '1.4.0',
     priority: 'medium',
   },
   {
     title: '[Ticket 4.2] Tableau de salaires : backend',
     description: ['[ ] Integrer la logique backend'],
     status: 'todo',
-    targetVersion: '1.3.0',
+    targetVersion: '1.4.0',
     priority: 'medium',
   },
   {
     title: '[Ticket 7] Impots : frontend',
     description: ['[ ] Integrer la logique frontend'],
     status: 'todo',
-    targetVersion: '1.4.0',
+    targetVersion: '1.5.0',
     priority: 'high',
   },
   {
@@ -28,7 +28,7 @@ export const DEV_TODO_ITEMS: DevTodoItem[] = [
       '[ ] Correction du calcul des transactions recurrentes hebdomadaires',
     ],
     status: 'in-progress',
-    targetVersion: '1.3.0',
+    targetVersion: '1.4.0',
     priority: 'medium',
   },
   {
@@ -36,7 +36,9 @@ export const DEV_TODO_ITEMS: DevTodoItem[] = [
     description: [
       "[X] Modal Nouvelle transaction : Ajouter la fonctionnalite d'ajout de sous-categories directement dans la liste deroulante des categories",
       '[ ] Modal Nouvelle transaction : Textebox montant - Avoir la meme coherence que celle de Nouvelle recurrence',
+      '[ ] Modal Nouvelle transaction : liste déroulante catégorie - Avoir la meme coherence que celle de Nouvelle recurrence',
       '[X] Modal Nouvelle transaction : Rendre une autocompletion intelligente pour la textbox "Description"',
+      '[ ] Modal Nouvelle transaction : Avoir la possibilité de taguer une transaction du compte courant comme "Avance compte joint" : attribuer une couleur differente dans le tableau de transactions; lui associer une icone "tick" cliquable dans la liste des transactions; quand on clique sur cette icone, la transaction est automatiquement déversée dans le compte joint et la transaction du compte courant est supprimée',
     ],
     status: 'in-progress',
     targetVersion: '1.3.0',
