655d17ade8389204edad42e06c0da0f878172e61
Djone <jonathan.guesnier@gmail.com>
2026-03-02 10:53:03 +0100

    Préparation pour le déploiement 1.3.0

--- frontend/src/app/version.ts
+++ frontend/src/app/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.3.0-dev';
-export const APP_BUILD_TIME = '2025-11-05T12:00:00Z';
+export const APP_VERSION = '1.4.0-dev-dev+16ab5d7';
+export const APP_BUILD_TIME = '2026-03-02T09:51:57.723Z';
 export const APP_ENV = 'development';
