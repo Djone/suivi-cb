2960e0719e6b1438994748658650ae8b5b22689f
Djone <jonathan.guesnier@gmail.com>
2026-05-26 14:32:01 +0200

    Update version numbers to 1.5.0 in configuration and environment files

--- frontend/src/app/version.ts
+++ frontend/src/app/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.4.5-dev-dev+ab5d899';
-export const APP_BUILD_TIME = '2026-04-01T09:38:21.843Z';
+export const APP_VERSION = '1.5.0-dev-dev+5afd4cb';
+export const APP_BUILD_TIME = '2026-04-07T09:58:11.515Z';
 export const APP_ENV = 'development';
