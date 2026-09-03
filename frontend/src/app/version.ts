0daa15a5578fa0b5c2702528210d97352432d622
Djone <jonathan.guesnier@gmail.com>
2026-09-03 12:19:33 +0200

    chore(release): prepare 1.6.5 and start 1.7.0-dev

--- frontend/src/app/version.ts
+++ frontend/src/app/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.7.0-dev-dev+7d99f6c';
-export const APP_BUILD_TIME = '2026-09-03T08:55:18.415Z';
+export const APP_VERSION = '1.7.0-dev-dev+761ee4e';
+export const APP_BUILD_TIME = '2026-09-03T10:19:33.707Z';
 export const APP_ENV = 'development';
